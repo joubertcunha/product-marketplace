889ed5d00191556e69fc099502f4a97a6004b83f
Joubert Cunha <joubertgoncalves@gmail.com>
2020-10-29 18:14:04 -0300

    criado projeto product-marketplace

---
+++ product-marketplace/src/main/java/br/com/challenge/ProductMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMarketplaceApplication.class, args);
+	}
+
+}
